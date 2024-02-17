84f60950be9a0ddd7be2ef11c30ce959a452858d
Krebs007 <159426675+Krebs007@users.noreply.github.com>
2024-02-17 17:50:10 +0300

    add some variable lesson:task

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a503236f7424ff3a6a580255a64c9bb33acd810d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
