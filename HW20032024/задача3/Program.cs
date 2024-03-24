1e2f88384a31c16ddeff21b8c8a80378ede46dd8
Krebs007 <159426675+Krebs007@users.noreply.github.com>
2024-03-24 16:53:21 +0300

    add HW-3

---
+++ HW20032024/задача3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        PrintArrayReverse(arr, arr.Length - 1);
+    }
+    
+    public static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(arr[index]);
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+}
+
+
